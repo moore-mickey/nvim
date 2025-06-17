f286f5cb56afb0731be433c0d9d60d60fac7285a
Mickey Moore <moore.mickey@outlook.com>
2025-06-16 21:55:24 -0400

    Initial commit

---
+++ init.lua
@@ -0,0 +1,24 @@
+print("advent of neovim")
+-- Mickey nvim
+
+require("custom.lazy")
+
+vim.opt.shiftwidth = 4
+vim.opt.clipboard = "unnamedplus"
+
+vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
+vim.keymap.set("n", "<space>x", ":.lua<CR>")
+vim.keymap.set("v", "<space>x", ":lua<CR>")
+
+-- Highlight when yanking (copying) text
+-- Try it with `yap` in normal mode
+-- See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking text',
+  group = vim.api.nvim_create_augroup('highlight-yank', {clear = true}),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+
