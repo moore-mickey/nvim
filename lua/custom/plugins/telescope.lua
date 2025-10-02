dd4feb1c02134a83033b1ce5879991641d276a34
bmoore <Brian.Moore@emp-corp.com>
2025-10-02 15:41:09 -0400

    Switch to telescope master

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -1,7 +1,8 @@
 return {
   {
     'nvim-telescope/telescope.nvim',
-    tag = '0.1.8',
+    branch = 'master',
+    -- tag = '0.1.8',
     dependencies = {
       'nvim-lua/plenary.nvim',
       { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
